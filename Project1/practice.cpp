eef38baf15f6d1d74742ac1fa508721ecca601b2
TaeHoon Yoon <willyepep@gmail.com>
2021-04-09 20:50:58 +0900

    template functor 연습

--- Project1/practice.cpp
+++ Project1/practice.cpp
@@ -1,42 +1,64 @@
 #include <iostream>
-#include <string>
+#include <initializer_list>
 using namespace std;
 
-size_t str_size(const string& str) {
-	return str.size();
-}
-size_t str_size(const char* str) {
-	return strlen(str);
-}
-template<typename String, typename... args>
-size_t str_size(const String& str, args... arg) {
-	return str_size(str) + str_size(arg...);
-}
+template<typename T>
+class vector {
+	T* data;
+	int size;
+	int res;
+public:
+	vector(int n):res(n),size(0),data(new T[n]){}
+	vector(const initializer_list<T>& li) {
+		res = li.size();
+		size = res;
+		data = new T[res];
+		int i = 0;
+		for (auto itr = li.begin(); itr != li.end(); i++, itr++) {
+			data[i] = *itr;
+		}
+	}
+	T& operator[](const int& idx) {
+		return data[idx];
+	}
+	
+	void push_back(T element) {
+		if (size >= res) {
+			T* tmp = new T[res * 2];
+			for (int i = 0; i < size; i++) {
+				tmp[i] = data[i];
+			}
+			res *= 2;
+			delete[] data;
+			data = tmp;
+		}
+		data[size] = element;
+		size++;
+	}
+	void swap(int i, int j) {
+		T tmp = data[i];
+		data[i] = data[j];
+		data[j] = tmp;
+	}
+	int len() {
+		return size;
+	}
+	~vector() {
+		delete[] data;
+	}
+};
+template<typename T>
+ostream& operator<<(ostream& os, const vector<T>& list) {
 
-void add_str(string* str) {
-	return;
 }
-template<typename String, typename... args>
-void add_str(string* source, const String& str, args... arg) {
-	source->append(string(str));
-	add_str(source, arg...);
-}
-template<typename String, typename... args>
-string Strcat(const String& str, args... arg) {
-	size_t tot_size = str_size(str, arg...);
-	string ret;
-	ret.reserve(tot_size);
-
-	ret.append(string(str));
-	add_str(&ret, arg...);
-	return ret;
-}
-
-
-
-
 int main() {
-	string tmp = "willy";
-	string str = Strcat(string("hello")," ","world", " ",tmp, string("!"));
-	cout << str << endl;
+
+	vector<int> list= {2, 3, 4, 5, 6, 7};
+	for (int i = 0; i < list.len(); i++) {
+		cout << list[i] << " ";
+	}
+	list.swap(0, 1);
+	for (int i = 0; i < list.len(); i++) {
+		cout << list[i] << " ";
+	}
 }
