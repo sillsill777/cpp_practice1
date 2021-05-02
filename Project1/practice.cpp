1650075db4ac39aa9b6273627433697f2facb5df
TaeHoon Yoon <willyepep@gmail.com>
2021-05-02 22:51:19 +0900

    classify prime meta programming

--- Project1/practice.cpp
+++ Project1/practice.cpp
@@ -1,33 +1,49 @@
 #include <iostream>
 using namespace std;
 
-template<int N, int M>
-struct GCD {
-	static const int res = GCD<M, N% M>::res;
-};
 template<int N>
-struct GCD<N, 0> {
-	static const int res = N;
+struct INT {
+	static const int data = N;
+};
+using one = INT<1>;
+using two = INT<2>;
+using three = INT<3>;
+
+template<typename N, typename M>
+struct add {
+	using result = INT<N::data + M::data>;
 };
 
-template<int N, int D=1>
-struct Ratio {
-private:
-	static const int gcd = GCD<N, D>::res;
-public:
-	static const int num = N / gcd;
-	static const int den = D / gcd;
+template<typename N, typename M>
+struct division {
+	using result = INT<N::data / M::data>;
 };
 
-
-
-template<typename op1, typename op2>
-struct _ratio_add {
-	using res = Ratio<op1::num* op2::den + op1::den * op2::num, op1::den* op2::den>;
+template<typename N, typename M>
+struct divide {
+	static const bool result = (N::data % M::data == 0) || divide<N, typename add<M, one>::result>::result;
 };
+
+template<typename N>
+struct divide<N, typename division<N, two>::result> {
+	static const bool result = (N::data % (N::data / 2)) == 0;
+};
+
+template<typename N>
+struct is_prime {
+	static const bool result = !(divide<N, two>::result);
+};
+
+template<>
+struct is_prime<two> {
+	static const bool result = true;
+};
+template<>
+struct is_prime<three> {
+	static const bool result = true;
+};
+
 int main() {
-	using a = Ratio<3, 2>;
-	using b = Ratio<3, 3>;
-	using c = _ratio_add<a, b>::res;
-	cout << c::num << "/" << c::den << endl;
+	is_prime<INT<61>>::result;
+
 }
